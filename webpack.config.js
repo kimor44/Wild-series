f39cdba5ab922c10d30ef62dbf5c091b59c9e6ee
Julien Guibert <julien.guib@gmail.com>
2019-11-16 19:11:48 +0100

    integration webpack, bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,10 @@ Encore
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
+    .configureSplitChunks(function(splitChunks) {
+    // change the configuration
+        splitChunks.minSize = 0;
+    })
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
@@ -54,7 +58,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
