ca3f7ef287385281f642248c9d0d98904c1af46c
Julien Guibert <julien.guib@gmail.com>
2019-11-18 16:07:17 +0100

    ajout wild/show/{slug} route

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -6,16 +6,35 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/wild", name="wild_")
+ */
 class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+     * @Route("/", name="index")
      */
     public function index() :Response
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
+    }
+
+    /**
+     * @Route("/show/{slug}", requirements={"slug"="[0-9a-z-]+"}, methods={"GET"}, defaults={"slug": "Aucune série sélectionnée, veuillez choisir une série"}, name="show")
+     * @param $slug
+     * @return Response
+     */
+    public function show($slug)
+    {
+        try{
+            return $this->render('wild/show.html.twig', [
+                'slug' => str_replace('-', ' ', ucwords($slug, '-')),
+            ]);
+        } catch (\Exception $e){
+            $e->getMessage("erreur 404");
+        }
     }
 
 }
