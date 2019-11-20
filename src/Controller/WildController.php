27dae52542aa3313ea3327cd3ce03434153e9087
Julien Guibert <julien.guib@gmail.com>
2019-11-20 16:30:51 +0100

    ajout category page avec fetch category

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Program;
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -34,7 +35,7 @@ class WildController extends AbstractController
      * @param string $slug The slugger
      * @return Response
      */
-    public function show(?string $slug):Response
+    public function show(string $slug): Response
     {
         if (!$slug) {
             throw $this->createNotFoundException('No slug has been sent to find a program in program\'s table.');
@@ -48,8 +49,32 @@ class WildController extends AbstractController
             throw $this->createNotFoundException('No program with ' . $slug . ' title, found in program\'s table.');
         }
         return $this->render('wild/show.html.twig', [
-            'program' => $program,
-            'slug' => $slug,
+            'program' => $program
+        ]);
+    }
+
+
+    /**
+     * Getting programs with a formatted slug for category
+     * @Route("/category/{categoryName}", requirements={"categoryName"="^[0-9a-z-]+$"}, defaults={"categoryName": null}, name="show_category")
+     * @param string $categoryName The category
+     * @return Response A category
+     */
+    public function showByCategory(string $categoryName): Response
+    {
+        if (!$categoryName) {
+            throw $this->createNotFoundException('No category has been sent to find programs in program\'s table.');
+        }
+        $categoryName = preg_replace(
+            '/-/',
+            ' ', trim(strip_tags($categoryName))
+        );
+        $category = $this->getDoctrine()->getRepository(Category::class)->findOneBy(['name' => mb_strtolower($categoryName)]);
+        if (!$category) {
+            throw $this->createNotFoundException('No programs with ' . $categoryName . ' category, found in category\'s table.');
+        }
+        return $this->render('wild/category.html.twig', [
+            'category' => $category,
         ]);
     }
 }
