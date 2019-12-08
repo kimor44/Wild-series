5a6f2773c612d430213f2a4a243839eaef9d3515
Julien Guibert <julien.guib@gmail.com>
2019-12-08 15:46:26 +0100

    ajout checkbox acteurs sur page ajout programme

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Actor;
 use App\Entity\Program;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -21,6 +22,13 @@ class ProgramType extends AbstractType
             ->add('actors', EntityType::class, [
                 'class' => Actor::class,
                 'choice_label' => 'name',
+                'expanded' => true,
+                'multiple' => true,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('a')
+                        ->orderBy('a.name', 'ASC');
+                },
+                'by_reference' => false,
             ]);
         ;
     }
